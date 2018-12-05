bb7f5f4cecd6202ea7a32b9b1a4ecad4bcfe5461
Terrance Li <terranceli.terbear@gmail.com>
2018-12-04 22:48:30 -0800

    Optimized Day 5

--- day-5/part1.js
+++ day-5/part1.js
@@ -4,27 +4,18 @@ aocLoader(
 	2018,
 	5,
 	'53616c7465645f5ff8b638d2c68c03cc3d780a40205335f819cd49e1e74f43b6b80111b52cbbaf243b1bf68109e5c3f4'
-).then(data => {
-	// data = require('fs').readFileSync('./test', 'utf8')
+).then(p => {
+	// p = require('fs').readFileSync('./test', 'utf8')
 
-	let polymer = data
-	let lastlength
-	do {
-		lastlength = polymer.length
-		for (let i = 0; i < polymer.length - 1; i++) {
-			if (
-				polymer.charAt(i).toUpperCase() === polymer.charAt(i + 1) &&
-				polymer.charAt(i) === polymer.charAt(i + 1).toLowerCase()
-			) {
-				polymer = polymer.slice(0, i) + polymer.slice(i + 2, polymer.length)
-				i--
-			} else if (
-				polymer.charAt(i).toLowerCase() === polymer.charAt(i + 1) &&
-				polymer.charAt(i) === polymer.charAt(i + 1).toUpperCase()
-			) {
-				polymer = polymer.slice(0, i) + polymer.slice(i + 2, polymer.length)
-				i--
-			}
+	for (let i = 0; i < p.length - 1; i++) {
+		if (
+			(p[i].toUpperCase() === p[i + 1] && p[i] === p[i + 1].toLowerCase()) ||
+			(p[i].toLowerCase() === p[i + 1] && p[i] === p[i + 1].toUpperCase())
+		) {
+			p = p.slice(0, i) + p.slice(i + 2, p.length)
+			i -= i === 0 ? 1 : 2
 		}
-	} while (polymer.length !== lastlength)
+	}
+
+	console.log(p.length)
 })
--- day-5/part2.js
+++ day-5/part2.js
@@ -4,47 +4,24 @@ aocLoader(
 	2018,
 	5,
 	'53616c7465645f5ff8b638d2c68c03cc3d780a40205335f819cd49e1e74f43b6b80111b52cbbaf243b1bf68109e5c3f4'
-).then(data => {
+).then(master => {
 	// data = require('fs').readFileSync('./test', 'utf8')
 
-	let polymer = data
+	let min = [...'abcdefghijklmnopqrstuvwxyz'].reduce((a, c) => {
+		let p = [...master.slice()].filter(k => k.toLowerCase() !== c.toLowerCase())
 
-	// part 2
-
-	const chars = 'abcdefghijklmnopqrstuvwxyz'
-	let lowest = polymer.length
-	;[...chars].forEach(c => {
-		console.log(c)
-		let copy = polymer.slice()
-		for (let i = 0; i < copy.length; i++) {
-			if (copy.charAt(i).toLowerCase() === c) {
-				copy = copy.slice(0, i) + copy.slice(i + 1, copy.length)
-				i--
+		for (let i = 0; i < p.length - 1; i++) {
+			if (
+				(p[i].toUpperCase() === p[i + 1] && p[i] === p[i + 1].toLowerCase()) ||
+				(p[i].toLowerCase() === p[i + 1] && p[i] === p[i + 1].toUpperCase())
+			) {
+				p.splice(i, 2)
+				i -= i === 0 ? 1 : 2
 			}
 		}
 
-		let lastlength
-		do {
-			lastlength = copy.length
-			for (let i = 0; i < copy.length - 1; i++) {
-				if (
-					copy.charAt(i).toUpperCase() === copy.charAt(i + 1) &&
-					copy.charAt(i) === copy.charAt(i + 1).toLowerCase()
-				) {
-					copy = copy.slice(0, i) + copy.slice(i + 2, copy.length)
-					i--
-				} else if (
-					copy.charAt(i).toLowerCase() === copy.charAt(i + 1) &&
-					copy.charAt(i) === copy.charAt(i + 1).toUpperCase()
-				) {
-					copy = copy.slice(0, i) + copy.slice(i + 2, copy.length)
-					i--
-				}
-			}
-		} while (copy.length !== lastlength)
+		return Math.min(a, p.length)
+	}, master.length)
 
-		lowest = copy.length < lowest ? copy.length : lowest
-	})
-
-	console.log(lowest)
+	console.log(min)
 })
