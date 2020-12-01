ae0f799a014a45a9c2936ea397fc23075e409486
Terrance Li <terranceli.terbear@gmail.com>
2020-11-30 22:21:21 -0800

    Added excel solution

--- 2020/day1/1.py
+++ 2020/day1/1.py
@@ -6,5 +6,7 @@ nums = list(map(lambda x: int(x), lines))
 for i in range(len(nums)):
   for j in range(i + 1, len(nums)):
     if nums[i] + nums[j] == 2020:
+      print(i, j)
+      print(nums[i],nums[j])
       print(nums[i] * nums[j])
       exit()
